0533663e12acb0b68597c89ddfaef1e2bca98497
InFTord <60395110+InFTord@users.noreply.github.com>
2020-08-17 13:46:21 +0500

    а)

---
+++ scripts/tab.js
@@ -0,0 +1,26 @@
+function openCity(evt, cityName) {
+    // Обьявление всех элементов
+    var i, tabcontent, tablinks;
+
+    // Нахождение элементов с классом tabcontent и их скрывание
+    tabcontent = document.getElementsByClassName("tabcontent");
+    for (i = 0; i < tabcontent.length; i++) {
+        tabcontent[i].style.display = "none";
+    }
+
+
+
+    // Нахождение всех элементов с классом tabcontent и удаление у них класса active
+    tablinks = document.getElementsByClassName("tablinks");
+    for (i = 0; i < tablinks.length; i++) {
+        tablinks[i].className = tablinks[i].className.replace(" active", "");
+    }
+
+    // Показывать вкладку которая открыта на данный момент и добавление к нему класса active
+    document.getElementById(cityName).style.display = "block";
+    evt.currentTarget.className += " active";
+    
+}
+
+// Нахождение элемента с id "defaultOpen" и нажатие на него
+    document.getElementById("defaultOpen").click();
